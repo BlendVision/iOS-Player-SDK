d0b66692bfb48734a33fd74540e9b8385ce763b5
Jay Lo <jaylo@kkstream.com>
2023-07-21 17:31:31 +0800

    Removed MilliSDK Package file udpated

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "BVPlayerUtilities",
+    name: "BVPlayer",
     defaultLocalization: "en",
     platforms: [
         .iOS(.v14)
@@ -12,22 +12,23 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BVPlayerUtilities",
+            name: "BVPlayer",
             targets: ["BVPlayer"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        //.package(url: "https://github.com/bitmovin/player-ios-core.git", exact: "2.4.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "BVPlayer",
-            dependencies: ["MillicastSDK", "HLSParser", "SwiftWebVTT"]),
+//        .target(
+//            name: "BVPlayer",
+//            dependencies: ["HLSParser", "SwiftWebVTT"]),
         .binaryTarget(
-            name: "MillicastSDK",
-            path: "Sources/BVPlayer/SDKs/MillicastSDK.xcframework"
+            name: "BVPlayer",
+            url: "https://github.com/BlendVision/iOS-Player-SDK/releases/download/2.4.1/KKSPlayer-2.4.1.42.xcframework.zip",
+            checksum: "7c87f0688c2dda3ad582839fb7df7c32c2d8a68b22e96a0089dcf59d01c7eaf7"
         ),
         .binaryTarget(
             name: "SwiftWebVTT",
