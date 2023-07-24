b68dad9259c09f89df4e4badc70772b242bed633
Jay Lo <jaylo@kkstream.com>
2023-07-24 16:13:21 +0800

    Updated Package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "BVPlayer",
-    defaultLocalization: "en",
     platforms: [
         .iOS(.v14)
     ],
@@ -17,14 +16,10 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        //.package(url: "https://github.com/bitmovin/player-ios-core.git", exact: "2.4.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .target(
-//            name: "BVPlayer",
-//            dependencies: ["HLSParser", "SwiftWebVTT"]),
         .binaryTarget(
             name: "BVPlayer",
             url: "https://github.com/BlendVision/iOS-Player-SDK/releases/download/2.4.1/KKSPlayer-2.4.1.42.xcframework.zip",
